f40438411dce38090737dfcc3295787c2a6d5820
Gurhan Batmaca <gurhanbtmc@hotmail.com>
2023-12-15 18:10:37 +0300

    home page product get method added

--- Data/Abstract/IProductRepository.cs
+++ Data/Abstract/IProductRepository.cs
@@ -4,6 +4,7 @@ namespace Data.Abstract
 {
     public interface IProductRepository: IRepository<Product>
     {
-        
+        Task<List<Product>> GetHomePageProducts(int page,int pageSize);
+        Task<int> GetHomePageProductsCount();
     }
 }
--- Data/Concrete/EfCore/EfCoreProductRepository.cs
+++ Data/Concrete/EfCore/EfCoreProductRepository.cs
@@ -1,5 +1,6 @@
 using Data.Abstract;
 using Entity;
+using Microsoft.EntityFrameworkCore;
 
 namespace Data.Concrete.EfCore
 {
@@ -10,6 +11,20 @@ namespace Data.Concrete.EfCore
         }
 
         protected ShopContext? Context => _context as ShopContext;
+
+        public async Task<List<Product>> GetHomePageProducts(int page,int pageSize)
+        {
+            var products = Context!.Products
+                                    .Where(i=>i.IsHome && i.IsApproved)
+                                    .AsQueryable();
+
+            return await products.Skip((page-1)*pageSize).Take(pageSize).ToListAsync();
+        }
+
+        public async Task<int> GetHomePageProductsCount()
+        {
+            return await Context!.Products.Where(i=>i.IsHome && i.IsApproved).CountAsync();
+        }
     }
     
 }
