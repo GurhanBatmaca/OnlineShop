ff47a2a05339a5518f10b7341f13d31a9c388c46
Gurhan Batmaca <gurhanbtmc@hotmail.com>
2023-12-19 21:13:28 +0300

    details safyası submit kapatıldı

--- Presentation/wwwroot/js/site.js
+++ Presentation/wwwroot/js/site.js
@@ -1,8 +1,23 @@
 ﻿// layout message toast script
 $(document).ready(function(){
     $("#toast").addClass("show");
-    setTimeout(function () { $("#toast").removeClass("show") },4000)
-})  
+    setTimeout(function () { 
+        $("#toast").removeClass("show") 
+    },4000);
+}) ; 
 
 // footer year script
 document.getElementById("year-footer").innerHTML = new Date().getFullYear();
+
+$(document).ready(function(){
+
+    $("#btn-submit").click(function(event){
+        event.preventDefault();
+
+        // $.ajax({
+        //     type: "POST",
+        //     url: Url.Acrion("")
+
+    });
+
+});
