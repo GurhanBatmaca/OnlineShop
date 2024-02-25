a596bd11504766664a3a76932f04d935f7e0d212
Gurhan Batmaca <gurhanbtmc@hotmail.com>
2024-02-25 21:04:53 +0300

    kategori navbar stil güncellendi

--- Presentation/wwwroot/js/site.js
+++ Presentation/wwwroot/js/site.js
@@ -16,5 +16,11 @@
     }
 }) ; 
 
+let categoryToggleBtn = document.getElementById("categoryToggleBtn");
+
+categoryToggleBtn.addEventListener("click",() => {
+    console.log("click")
+});
+
 // footer year script
 document.getElementById("year-footer").innerHTML = new Date().getFullYear();
